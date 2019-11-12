4e53617ad11d38c6c318103ddb0ea569a3722763
Fabian Sauter <sauter.fabian@mailbox.org>
2019-11-12 17:51:41 +0100

    Added a dummy XMPP credentials store

---
+++ ESP32/main/xmpp/XmppCredentials.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+//---------------------------------------------------------------------------
+namespace espiot::xmpp {
+//---------------------------------------------------------------------------
+const std::string JID = "device@example.org";
+const std::string JID_PASSWORD = "Some Password";
+const std::string JID_SENDER = "sender@example.org";
+const std::string SERVER_IP = "0.0.0.0";
+const uint16_t SERVER_PORT = 5222;
+//---------------------------------------------------------------------------
+} // namespace espiot::xmpp
+//---------------------------------------------------------------------------
