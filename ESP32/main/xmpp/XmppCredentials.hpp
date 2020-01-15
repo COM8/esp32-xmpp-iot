89815fb56fa9ce8ed79e746811a7a2007e04c02a
Fabian Sauter <sauter.fabian@mailbox.org>
2020-01-15 16:38:49 +0100

    One more time, but with feeling.

--- ESP32/main/xmpp/XmppCredentials.hpp
+++ ESP32/main/xmpp/XmppCredentials.hpp
@@ -5,14 +5,10 @@
 //---------------------------------------------------------------------------
 namespace espiot::xmpp {
 //---------------------------------------------------------------------------
-const std::string JID = "";
-const std::string JID_PASSWORD = "";
-<<<<<<< HEAD
-const std::string JID_SENDER = "s";
-=======
-const std::string JID_SENDER = "";
->>>>>>> 0b0916547b2b557a4754bd4440b54bee620d953e
-const std::string SERVER_IP = "";
+const std::string JID = "iot1@xmpp2.uwpx.org";
+const std::string JID_PASSWORD = "iot1";
+const std::string JID_SENDER = "com8@xmpp.uwpx.org";
+const std::string SERVER_IP = "xmpp2.uwpx.org";
 const uint16_t SERVER_PORT = 5222;
 //---------------------------------------------------------------------------
 } // namespace espiot::xmpp
